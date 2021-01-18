b110c451411c70b8e07d8a23ffd1023409d09bec
Mngz <mngz636@gmail.com>
2021-01-18 13:53:32 +0200

    Update product.js

--- product.js
+++ product.js
@@ -90,14 +90,14 @@ function rinse_ali_fields(ind){
 			
 			
 			if(fields[a].nextElementSibling.innerHTML=='attrValue'){
-				   if(/"[\w\s]*"/.test(fields[a].value)){
-				   fields[1].value+=fields[a].value+'<br>';
+				   if(/".*"/.test(fields[a].value)){
+				   fields[0].value+=fields[a].value+'<br>';
 				   }
 			}else if(fields[a].nextElementSibling.innerHTML=='ogTitle'){
-				   fields[1].value+=fields[a].value+'<br><br>';
+				   fields[0].value+=fields[a].value+'<br><br>';
 			}else if(fields[a].nextElementSibling.innerHTML=='description'){
 				   if(/"[\w\s]*"/.test(fields[a].value)){
-				   fields[0].value+=fields[a].value;
+				   fields[1].value+=fields[a].value;
 				   }   
 			}
 			
