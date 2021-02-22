3bd53204d7f52104c83376f84cda1714e562796a
Mngz <mngz636@gmail.com>
2021-02-22 03:07:28 +0200

    Create s_plug.js

---
+++ aliexpress-product-scraper-master/s_plug.js
@@ -0,0 +1,55 @@
+const scrape = require('index.js');
+
+var product;
+
+function setScrape(id){
+	product = scrape(id);
+}
+
+function getAliProductTitle(){
+	var tt;
+
+product.then(res => {
+  res.map(function(pp) {
+	tt = pp.title;
+	});	
+});  
+	return tt;  
+}
+
+function getAliProductDescription(){
+var dd;
+	
+product.then(res => { 
+  res.map(function(pp) {
+	dd = pp.description;
+	});
+});  
+	return dd;
+}
+
+function getAliProductFeedback(){
+ var cc = '';
+ 
+product.then(res => {
+  res.map(function(ppp) {
+	
+	  var ff = ppp.feedback;
+	  
+	  for(var a=0;a<ff.length;a++){
+		  
+	var pp = ppp.feedback[a].photos;
+	  var photos = '';
+	  
+	  for(var aa=0;aa<pp.length;aa++){
+		photos+= '<img src="'+pp[aa]+'" />';
+	  }
+		cc +=  ppp.feedback[a].displayName+'('+ppp.feedback[a].rating+'<small>Star Rating</small>)<br>: '+ppp.feedback[a].content+' <br>'+photos+';;';
+	  }
+	  
+	
+	});
+});  
+
+return cc;
+}
