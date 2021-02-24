42aa2b7c3b98d7a9f462ee340452f208393dd458
Mngz <mngz636@gmail.com>
2021-02-24 03:26:10 +0200

    Update aliexpressProductScraper.js

--- aliexpress-product-scraper-master/src/aliexpressProductScraper.js
+++ aliexpress-product-scraper-master/src/aliexpressProductScraper.js
@@ -4,34 +4,45 @@ const cheerio = require('cheerio');
 const Variants = require('./variants');
 const Feedback = require('./feedback');
 
+//async
 function AliexpressProductScraper(productId, feedbackLimit) {
   const FEEDBACK_LIMIT = feedbackLimit || 10;
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  
+                  //await
+  const browser = puppeteer.launch();
+                //await
+  const page = browser.newPage();
 
   /** Scrape the aliexpress product page for details */
-  await page.goto(`https://www.aliexpress.com/item/${productId}.html`);
-  const aliExpressData = await page.evaluate(() => runParams);
+  //await
+   page.goto(`https://www.aliexpress.com/item/${productId}.html`);
+                           //await
+  const aliExpressData =  page.evaluate(() => runParams);
 
   const data = aliExpressData.data;
 
   /** Scrape the description page for the product using the description url */
   const descriptionUrl = data.descriptionModule.descriptionUrl;
-  await page.goto(descriptionUrl);
-  const descriptionPageHtml = await page.content();
+  //await
+  page.goto(descriptionUrl);
+                              //await
+  const descriptionPageHtml =  page.content();
 
   /** Build the AST for the description page html content using cheerio */
   const $ = cheerio.load(descriptionPageHtml);
   const descriptionData = $('body').html();
 
   /** Fetch the adminAccountId required to fetch the feedbacks */
-  const adminAccountId = await page.evaluate(() => adminAccountId);
-  await browser.close();
+                          //await
+  const adminAccountId = page.evaluate(() => adminAccountId);
+  //await 
+ browser.close();
 
   let feedbackData = [];
 
   if (data.titleModule.feedbackRating.totalValidNum > 0) {
-    feedbackData = await Feedback.get(
+                   //await
+    feedbackData = Feedback.get(
       data.actionModule.productId,
       adminAccountId,
       data.titleModule.feedbackRating.totalValidNum,
