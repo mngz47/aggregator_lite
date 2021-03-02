f159254fbb2d3b38d4efc2b5132485337b152afd
Mngz <mngz636@gmail.com>
2021-03-02 16:07:01 +0200

    Update plug.php

--- plug.php
+++ plug.php
@@ -1,3 +1,3 @@
 <?php
-echo ($_GET['category']?'<script>window.open("https://www.productlists.co.za/feature/aggregation/lite/index.html?category='.$_GET['category'].'");</script>':'');
+echo ($_GET['category']?'<script>window.open("https://www.productlists.co.za/feature/aggregation/lite/index.html?category='.$_GET['category'].'&page=0");</script>':'');
 ?>
