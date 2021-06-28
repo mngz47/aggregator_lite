7a5590e627d9b5f762057215a88828c0960af33f
Mngz <mngz636@gmail.com>
2021-06-28 03:54:23 +0200

    Update is_duplicate.php

--- is_duplicate.php
+++ is_duplicate.php
@@ -2,7 +2,8 @@
 
 if($_POST['aff_link']){
 
-$conn = new mysqli('localhost','produc10_mng','mngzpass636','produc10_productlists');
+$conn = new mysqli('d6rii63wp64rsfb5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','muce70z5ukkwpv5d','bqvn5jp04wlmxu64','zyo1oodysira7ro5');
+	
 $sql = 'SELECT id FROM product WHERE CONTAINS(refs,"'.$_POST['aff_link'].'")';
 $result = $conn->query($sql);
 	     if($result){
